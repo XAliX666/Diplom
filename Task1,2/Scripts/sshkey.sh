a25cb5180f5f28a4687c697bc8ef7a958fc0fde8
Gamzaev <mamedzade@MacBook-Air-Pobeda.local>
2023-01-26 21:17:38 +0300

    Add sshkey

--- Task1,2/Scripts/sshkey.sh
+++ Task1,2/Scripts/sshkey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#First query enter enter, Second query enter servername, Threes query enter passwd servername
+#First query enter enter, Second query enter servername
 read -r -p "Enter file in which to save the key (/Users/siur/.ssh/id_rsa):" enter
 read -r -p "Enter Who is sent pub.key(ali@123.123.1331.21): " servername
 #Create keys and passphrasa(by hand)
