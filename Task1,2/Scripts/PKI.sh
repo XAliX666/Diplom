8495e462cf50b62ffc90a00898d635eff369ae70
Gamzaev <mamedzade@MacBook-Air-Pobeda.local>
2023-01-27 16:42:40 +0300

    as

--- Task1,2/Scripts/PKI.sh
+++ Task1,2/Scripts/PKI.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+#Install EASY_RSA 
+read -r -p "Password for sudo: " passwd
+echo $passwd | sudo -S apt-get update
+echo $passwd | sudo -S apt-get install easy-rsa
+ 
+dpkg -s easy-rsa &>> /dev/null
+if [ $? -eq 0 ]
+then
+  echo "Successfully install easy-rsa"
+else
+  echo "No intall easy-rsa >&2
+fi
